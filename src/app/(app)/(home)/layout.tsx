b7cd1ef3c49f8e6d830b4a79345392a9ffeebbc7
RustySkeleton <adityarajsharma100@gmail.com>
2026-06-10 17:53:51 +0530

    07: TRPC Integration

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,12 +1,13 @@
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
-import { Category } from '@/payload-types';
+
 
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
-import { SearchFilters } from "./search-filters";
+import { SearchFilters, SearchFiltersLoading } from "./search-filters";
 import dns from 'dns'; 
-import { CustomCategory } from './types';
+
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from "react";
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 interface Props{
@@ -14,33 +15,19 @@ interface Props{
 };
 
 const Layout = async ({children}:Props) => {
-    const payload = await getPayload({
-        config: configPromise,
-      });
-    const data = await payload.find({
-        collection: 'categories',
-        depth:1, // Populate subcategories.[0] will be a type of "Category"
-        pagination:false,
-        where:{
-        parent:{
-            exists:false,
-            },
-        },
-        sort:"name"
-    });
-    const formattedData:CustomCategory[]=data.docs.map((doc)=>({
-        ...doc,
-        subcategories:(doc.subcategories?.docs ?? []).map((doc)=>({
-            //Because of depth:1 we are confident doc will be a type of Category
-            ...(doc as Category),
-            subcategories:undefined,
-        }))
-    }));
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(
+        trpc.categories.getMany.queryOptions(),
+    );
 
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar/>
-            <SearchFilters data={formattedData}/>
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<SearchFiltersLoading/>}>
+                    <SearchFilters/>
+                </Suspense>
+            </HydrationBoundary>
             <div className="flex-1 bg-[#F4F4F0]">
                 {children}
 
