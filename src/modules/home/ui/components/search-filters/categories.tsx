7c63918bc2ea9219c1ebda1621d7530b1797a553
RustySkeleton <adityarajsharma100@gmail.com>
2026-06-16 21:00:30 +0530

    10: Category Pages

--- src/modules/home/ui/components/search-filters/categories.tsx
+++ src/modules/home/ui/components/search-filters/categories.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+
 import { useEffect, useRef, useState } from "react";
 
 import { CategoryDropDown } from "./category-dropdown";
@@ -9,12 +11,16 @@ import { cn } from "@/lib/utils";
 import { ListFilterIcon } from "lucide-react";
 import { CategoriesSidebar } from "./categories-sidebar";
 import { CategoriesGetManyOutput } from "@/modules/categories/types";
+import { useParams } from "next/navigation";
 
 interface Props{
     data:CategoriesGetManyOutput;
 };
 
 export const Categories =({data}:Props)=>{
+    const params = useParams();
+
+
     const containerRef = useRef<HTMLDivElement>(null);
     const measureRef = useRef<HTMLDivElement>(null);
     const viewAllRef = useRef<HTMLDivElement>(null);
@@ -22,8 +28,10 @@ export const Categories =({data}:Props)=>{
     const [visibleCount, setVisibleCount] =useState(data.length);
     const [isAnyHovered, setIsAnyHovered]= useState(false);
     const [isSidebarOpen, setIsSidebarOpen]=useState(false);
-    
-    const activeCategory="all";
+
+    const categoryParam=params.category as string | undefined;
+
+    const activeCategory=categoryParam || "all";
 
     const activeCategoryIndex = data.findIndex((cat)=>cat.slug === activeCategory);
     const isActiveCategoryHidden = activeCategoryIndex>=visibleCount && activeCategoryIndex!==-1;
@@ -94,6 +102,7 @@ export const Categories =({data}:Props)=>{
 
             <div ref={viewAllRef} className="shrink-0">
                 <Button 
+                variant="elevated"
                 className={cn("h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
                             isActiveCategoryHidden && !isAnyHovered && "bg-white border-primary",)}
                 onClick={()=>setIsSidebarOpen(true)}
