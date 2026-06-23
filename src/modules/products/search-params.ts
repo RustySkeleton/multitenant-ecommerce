71f642df9385eb62bac55819e51a34f61e02f348
RustySkeleton <adityarajsharma100@gmail.com>
2026-06-23 19:29:30 +0530

    13: API Filters and Sorting

--- src/modules/products/search-params.ts
+++ src/modules/products/search-params.ts
@@ -1,6 +1,7 @@
-import { useQueryStates,parseAsString,parseAsArrayOf, parseAsStringLiteral} from "nuqs";
+import {parseAsString,createLoader,parseAsArrayOf, parseAsStringLiteral} from "nuqs/server";
 
-const sortValues = ["curated","trending","hot_and_new"] as const;
+
+export const sortValues = ["curated","trending","hot_and_new"] as const;
 
 const params = {
     sort: parseAsStringLiteral(sortValues).withDefault("curated"),
@@ -23,7 +24,4 @@ const params = {
         .withDefault([]),
 }
 
-export const useProductFilters= () =>{
-    return useQueryStates(params);
-}; 
-
+export const loadProductFilters =createLoader(params);
