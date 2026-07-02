dffc3a19b43df80fb81524fece3c411edd27c053
RustySkeleton <adityarajsharma100@gmail.com>
2026-07-02 22:23:35 +0530

    16: Tenants Pages

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 import {baseProcedure, createTRPCRouter} from "@/trpc/init"
 import type { Sort, Where } from "payload";
-import { Category, Media } from "@/payload-types";
+import { Category, Media, Tenant } from "@/payload-types";
 import { sortValues } from "../search-params";
 import { DEFAULT_LIMIT } from "@/constants";
 
@@ -18,6 +18,7 @@ export const productsRouter = createTRPCRouter({
                 maxPrice: z.string().nullable().optional(),
                 tags: z.array(z.string()).nullable().optional(),
                 sort:z.enum(sortValues).nullable().optional(),
+                tenantSlug: z.string().nullable().optional(),
             }),
         )
         .query(async ({ctx,input})=>{
@@ -48,6 +49,12 @@ export const productsRouter = createTRPCRouter({
                     less_than_equal: input.maxPrice
                 }
             }
+
+            if (input.tenantSlug){
+                where["tenant.slug"]={
+                    equals:input.tenantSlug,
+                };
+            };
 
             if(input.category){
                 const categoriesData = await ctx.db.find({
@@ -89,12 +96,12 @@ export const productsRouter = createTRPCRouter({
             }
 
             const data = await ctx.db.find({
-            collection: 'products',
-            depth:1, // Populate "category" and "image"
-            where,
-            sort,
-            page:input.cursor,
-            limit:input.limit,
+                collection: 'products',
+                depth:2, // Populate "category", "image", "tenant", "tenant.image"
+                where,
+                sort,
+                page:input.cursor,
+                limit:input.limit,
             });
 
             return {
@@ -102,6 +109,7 @@ export const productsRouter = createTRPCRouter({
                 docs:data.docs.map((doc)=>({
                     ...doc,
                     image:doc.image as Media | null,
+                    tenant: doc.tenant as Tenant & {image:Media | null},
                 }))
             };
     }),
