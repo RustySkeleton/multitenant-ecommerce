f3ba1f4a2caa02c24825200babb1ca1480231b2e
RustySkeleton <adityarajsharma100@gmail.com>
2026-07-05 20:49:29 +0530

    17: Product Page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,10 @@ export function generateTenantURL(tenantSlug:string){
   return `/tenants/${tenantSlug}`;
 };
 
+export function formatCurrency(value:number|string){
+  return new Intl.NumberFormat("en-US",{
+      style:"currency",
+      currency:"USD",
+      maximumFractionDigits:0
+  }).format(Number(value))
+}
