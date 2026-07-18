474cfe5975e7dc161e44b3782d3fcba1e69a08bb
RustySkeleton <adityarajsharma100@gmail.com>
2026-07-18 21:55:03 +0530

    20: Stripe Integration

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,7 +1,8 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
 import { getPayload } from 'payload';
 import config from "@payload-config";
 import superjson from 'superjson';
+import { headers as getHeaders } from "next/headers";
 import { cache } from 'react';
 import dns from 'dns'; dns.setServers(['8.8.8.8', '8.8.4.4']);
 export const createTRPCContext = cache(async () => {
@@ -29,4 +30,24 @@ export const baseProcedure = t.procedure.use(async ({ next }) => {
   const payload = await getPayload({ config });
 
   return next({ ctx: { db: payload } });
+});
+
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
+  const headers = await getHeaders();
+  const session = await ctx.db.auth({ headers });
+  if (!session.user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Not authenticated",
+    });
+  }
+  return next({
+    ctx: {
+      ...ctx,
+      session: {
+        ...session,
+        user: session.user,
+      },
+    },
+  });
 });
