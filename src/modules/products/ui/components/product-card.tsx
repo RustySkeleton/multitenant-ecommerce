ccbf5146c1f53504ef16f75af7760aa6effdd947
RustySkeleton <adityarajsharma100@gmail.com>
2026-08-03 18:40:51 +0530

    23: Aggregating Reviews

--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-// TODO: Add real ratings
 
 interface ProductCardProps {
     id: string;
